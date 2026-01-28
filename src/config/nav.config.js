092dea2230edc60cbe44d95eb15c5bf05c48357e
Grant <fishkq@aliyun.com>
2026-01-28 08:15:19 +0800

    Update nav.config.js

--- src/config/nav.config.js
+++ src/config/nav.config.js
@@ -61,8 +61,8 @@ export const navConfig = {
       name: '公众号',
       icon: '🎨',
       items: [
-        { name: 'Wechat-MP', url: 'https:/hapgpt.com/mp', desc: '公众号图片排版', icon: '🐙' },
-        { name: 'Wechat文章', url: 'https:/hapgpt.com/mp1', desc: '公众号文字排版', icon: '💬' },
+        { name: 'Wechat-MP', url: 'https://hapgpt.com/mp', desc: '公众号图片排版', icon: '🐙' },
+        { name: 'Wechat文章', url: 'https://hapgpt.com/mp1', desc: '公众号文字排版', icon: '💬' },
         { name: '废话生成器', url: 'http://cooker.github.io/wx-mp-dog', desc: '废话生成器', icon: '💬' },
       ],
     },
